f54398acaca34b2fb627bb2d29ab47a886f1bbd0
= <randyjp125@gmail.com>
2017-11-13 08:40:27 -0500

    adding run game function to test pygame

--- src/app.py
+++ src/app.py
@@ -1,4 +1,23 @@
+import sys
 import pygame
 
-print ('hello worldxx')
-print ('hello world2222')
+
+def run_game():
+    # Initialize game and create a screen object.
+
+    pygame.init()
+
+    screen = pygame.display.set_mode((1200, 800))
+    pygame.display.set_caption("Alien Invasion")
+
+    # Start the main loop for the game.
+    while True:
+        # Watch for keyboard and mouse events.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+    # Make the most recently drawn screen visible.
+    pygame.display.flip()
+
+
+run_game()
