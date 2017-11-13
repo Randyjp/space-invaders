579462d1195a3332463ed294c8b0d2f97aac8aa0
= <randyjp125@gmail.com>
2017-11-13 08:51:00 -0500

    changing background color

--- src/app.py
+++ src/app.py
@@ -10,14 +10,20 @@ def run_game():
     screen = pygame.display.set_mode((1200, 800))
     pygame.display.set_caption("Alien Invasion")
 
+    # Set the background color
+    bg_color = (230, 230, 230)
+
     # Start the main loop for the game.
     while True:
         # Watch for keyboard and mouse events.
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-    # Make the most recently drawn screen visible.
-    pygame.display.flip()
+        # Redraw the screen after each pass through loop
+        screen.fill(bg_color)
+
+        # Make the most recently drawn screen visible.
+        pygame.display.flip()
 
 
 run_game()
