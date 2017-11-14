c2ef45d619cc0cdc483b3437d9fcd11c9d14bcaf
= <randyjp125@gmail.com>
2017-11-14 08:40:57 -0500

    creating settings class

--- src/app.py
+++ src/app.py
@@ -1,17 +1,15 @@
 import sys
 import pygame
 
+from settings import Settings
+
 
 def run_game():
     # Initialize game and create a screen object.
-
     pygame.init()
-
-    screen = pygame.display.set_mode((1200, 800))
+    ai_setting = Settings()
+    screen = pygame.display.set_mode((ai_setting.screen_width, ai_setting.screen_height))
     pygame.display.set_caption("Alien Invasion")
-
-    # Set the background color
-    bg_color = (230, 230, 230)
 
     # Start the main loop for the game.
     while True:
@@ -20,7 +18,7 @@ def run_game():
             if event.type == pygame.QUIT:
                 sys.exit()
         # Redraw the screen after each pass through loop
-        screen.fill(bg_color)
+        screen.fill(ai_setting.bg_color)
 
         # Make the most recently drawn screen visible.
         pygame.display.flip()
