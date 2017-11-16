d208ff8477be75a0c3e65e6e239af83267314203
= <randyjp125@gmail.com>
2017-11-16 09:02:21 -0500

    creating one alien

---
+++ src/alien.py
@@ -0,0 +1,27 @@
+import pygame
+from pygame.sprite import Sprite
+
+
+class Alien(Sprite):
+    """A class to represent a single alien in the fleet"""
+
+    def __init__(self, ai_settings, screen):
+        """""Initialize the alien and set it on the starting position"""
+        self.screen = screen
+        self.ai_settings = ai_settings
+
+        # Load the image and get the alien's rect
+        self.image = pygame.image.load('images/alien.bmp')
+        self.rect = self.image.get_rect()
+        # self.screen_rect = self.screen.get_rect()
+
+        # start each alien at the top left
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
+
+        # Store Alien's exact position
+        self.x = float(self.rect.x)
+
+    def blitme(self):
+        """Draw the alien"""
+        self.screen.blit(self.image, self.rect)
--- src/game_functions.py
+++ src/game_functions.py
@@ -15,6 +15,8 @@ def check_keydown_event(event, ai_settings, screen, ship, bullets):
         # New bullet, goes to the bullet group
         new_bullet = Bullet(ai_settings, screen, ship)
         bullets.add(new_bullet)
+    elif event.key == pygame.K_q:
+        sys.exit()
 
 
 def check_keyup_event(event, ship):
@@ -38,7 +40,7 @@ def check_events(ai_settings, screen, ship, bullets):
             check_keyup_event(event, ship)
 
 
-def update_screen(ai_settings, screen, ship, bullets):
+def update_screen(ai_settings, screen, ship, alien, bullets):
     """Update images on screen and flip to the new screen"""
     # Redraw the screen after each pass through loop
     screen.fill(ai_settings.bg_color)
@@ -47,6 +49,7 @@ def update_screen(ai_settings, screen, ship, bullets):
         bullet.draw_bullet()
 
     ship.blitme()
+    alien.blitme()
 
     # Make the most recently drawn screen visible.
     pygame.display.flip()
